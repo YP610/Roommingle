cde24512886a58eaafb570a578673f98e240751b
jonathanliu72 <jonathan.liu72@gmail.com>
2025-02-17 00:14:26 -0500

    imported mongoose,cors, and dotenv

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,9 @@
+//imports required packages that we installed through the terminal
 const express = require("express");
+const mongoose = require('mongoose');
+const cors = require('cors');
+require('dotenv').config();
+
 const app = express();
 const PORT = process.env.PORT || 1000;
 
