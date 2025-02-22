4c97e236f4ec282a436e2b2df04d75395f6ca31d
jonathanliu72 <jonathan.liu72@gmail.com>
2025-02-22 13:12:15 -0500

    added API routes

--- server/routes/students.js
+++ server/routes/students.js
@@ -1,7 +1,33 @@
 const express = require('express')
 
-const router = express.Router()
+const router = express.Router() //creates router
 
-router.get('/', () => {})
+//adds all request handlers to router
 
+// GET all students
+router.get('/', (req, res) => {
+    res.json({mssg: 'GET all students'})
+})
+
+// Get a single student
+router.get('/:id', (req, res) => {
+    res.json({mssg: 'GET a single student'})
+})
+
+// POST a new student
+router.post('/', (req, res) => {
+    res.json({mssg: 'POST a new student'})
+})
+
+// DELETE a new student
+router.delete('/:id', (req, res) => {
+    res.json({mssg: 'DELETE a new student'})
+})
+
+// UPDATE a student
+router.patch('/:id', (req, res) => {
+    res.json({mssg: 'UPDATE a student'})
+})
+
+//exports router to server.js
 module.exports = router
