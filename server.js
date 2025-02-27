a9ff6a2381c01d888c6c1b38d0eb69e66c7e7228
jonathanliu72 <jonathan.liu72@gmail.com>
2025-02-26 23:13:54 -0500

    Add files via upload

--- server.js
+++ server.js
@@ -3,15 +3,14 @@ const express = require("express");
 const studentRoutes = require("./routes/students") //requires API routes
 const mongoose = require('mongoose');
 const cors = require('cors');
-//attaches env variables to processed env objects
-require('dotenv').config();
-//connect to mongodb
-const dbURI='mongodb+srv://pranav:Pg@cluster0.dcxpq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
-mongoose.connect(dbURI);
 
 //express app
 const app = express();
 const PORT = process.env.PORT;
+
+//attaches env variables to processed env objects
+require('dotenv').config();
+
 
 //middleware
 app.use(express.json())
@@ -28,9 +27,19 @@ app.get("/", (req, res) => {
 // uses all the request handlers imported from students.js (must include /api/students route)
 app.use('/api/students', studentRoutes)
 
+//connect to mongodb
+const dbURI='mongodb+srv://pranav:Pg@cluster0.dcxpq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
+mongoose.connect(dbURI)
+    .then(() => {
+        //listens for requests
+        app.listen(PORT, () => {
+        console.log('connected to db & listening on port', PORT);
+        });
+    })
+    .catch((error) => {
+        console.log(error)
+        res.send("Hello")
+    })
 
 
-//listens for requests
-app.listen(PORT, () => {
-    console.log('listening on port', PORT);
-});
+
