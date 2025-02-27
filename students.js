5f0634f4c75e782a705ed80a0caa7eb6b58dcd43
YP10 <144463548+YP10@users.noreply.github.com>
2025-02-26 21:45:03 -0500

    stuff

--- students.js
+++ students.js
@@ -1,5 +1,5 @@
 const express = require('express')
-
+const User=require('../Schema/userSchema')
 const router = express.Router() //creates router
 
 //adds all request handlers to router
@@ -15,8 +15,15 @@ router.get('/:id', (req, res) => {
 })
 
 // POST a new student
-router.post('/', (req, res) => {
-    res.json({mssg: 'POST a new student'})
+router.post('/', async (req, res) => {
+    const {name,email,password}=req.body
+    try{
+        const user= await User.create({name,email,password})
+        res.status(200).json({user})
+    } catch(error){
+        res.status(400).json({error: error.message})
+    }
+
 })
 
 // DELETE a new student
