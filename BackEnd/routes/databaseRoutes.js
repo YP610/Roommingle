a9e8b8376d26562de4b641a9f52613b2340f18e6
jonathanliu72 <jonathan.liu72@gmail.com>
2025-03-21 02:37:19 -0400

    replaced router functions with references to controllers

--- BackEnd/routes/databaseRoutes.js
+++ BackEnd/routes/databaseRoutes.js
@@ -1,40 +1,29 @@
 const express = require('express')
-const User=require('../models/userSchema')
+const {
+    getUsers,
+    getUser,
+    createUser,
+    deleteUser,
+    updateUser
+} = require("../controllers/userController")
 const router = express.Router() //creates router
 
 //adds all request handlers to router
 
 // GET all users
-router.get('/', (req, res) => {
-    res.json({mssg: 'GET all users'})
-})
+router.get('/', getUsers)
 
 // Get a single user
-router.get('/:id', (req, res) => {
-    res.json({mssg: 'GET a single user'})
-})
+router.get('/:id', getUser)
 
 // POST a new user
-router.post('/', async (req, res) => {
-    const {name,email,password}=req.body
-    try{
-        const user= await User.create({name,email,password})
-        res.status(200).json({user})
-    } catch(error){
-        res.status(400).json({error: error.message})
-    }
-
-})
+router.post('/', createUser)
 
 // DELETE a new user
-router.delete('/:id', (req, res) => {
-    res.json({mssg: 'DELETE a new user'})
-})
+router.delete('/:id', deleteUser)
 
 // UPDATE a user
-router.patch('/:id', (req, res) => {
-    res.json({mssg: 'UPDATE a user'})
-})
+router.patch('/:id', updateUser)
 
 //exports router to server.js
 module.exports = router
