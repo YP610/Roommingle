0f983f8ea137643733db6e1f55b393cb67326517
YP10 <144463548+YP10@users.noreply.github.com>
2025-03-30 22:18:17 -0400

    Fixed server

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -1,10 +1,11 @@
 //imports required packages that we installed through the terminal
 require('dotenv').config();
 const express = require("express");
-const userConnections = require('./routes/userRoutes'); 
-const livingConnections=require('./routes/livingConditionsRoutes');
-const contactConnections=require('./routes/contactRoutes');
-const hobbyConnections=require('./routes/hobbyRoutes');
+const userRoutes = require('./routes/userRoutes'); 
+const LcRoutes=require('./routes/livingConditionsRoutes');
+const contactRoutes=require('./routes/contactRoutes');
+const feedRoutes=require('./routes/feedRoutes');
+const hobbyRoutes=require('./routes/hobbyRoutes');
 //requires API routes
 const authRoutes = require('./routes/authRoutes');
 const cors = require('cors');
@@ -28,11 +29,12 @@ app.get("/", (req, res) => {
 });//just a test (delete later)
 
 // uses all the request handlers imported from students.js (must include /api/students route)
-app.use('/api/userRoutes', userConnections)
+app.use('/api/userRoutes', userRoutes)
 app.use('/api/auth',authRoutes);
-app.use('/api/livingConditionsRoutes',livingConnections)
-app.use('/api/contactRoutes',contactConnections)
-app.use('/api/hobbyRoutes',hobbyConnections)
+app.use('/api/livingConditionsRoutes',LcRoutes)
+app.use('/api/feedRoutes',feedRoutes)
+app.use('/api/contactRoutes',contactRoutes)
+app.use('/api/hobbyRoutes',hobbyRoutes)
 //connect to mongodb
 connectDB()
     .then(() => {
