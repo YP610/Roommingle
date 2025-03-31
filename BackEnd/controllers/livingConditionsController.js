d5e7c1df272e13a9d5d469eaef686935cd825171
Pranav_Gaddam <116840364+Pcoder6@users.noreply.github.com>
2025-03-30 21:29:55 -0400

    Living Conditions Controller file

---
+++ BackEnd/controllers/livingConditionsController.js
@@ -0,0 +1,68 @@
+const LivingConditions = require("../models/livingConditionsSchema");
+const mongoose = require("mongoose");
+
+// Get all living conditions
+const getLivingConditions = async (req, res) => {
+    const conditions = await LivingConditions.find({}).sort({ createdAt: -1 }).populate('user');
+    res.status(200).json(conditions);
+};
+
+// Get a single living condition
+const getLivingCondition = async (req, res) => {
+    const { id } = req.params;
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+        return res.status(404).json({ error: 'No such living condition' });
+    }
+    const condition = await LivingConditions.findById(id).populate('user');
+    if (!condition) {
+        return res.status(404).json({ error: "No such living condition" });
+    }
+    res.status(200).json(condition);
+};
+
+// Create a new living condition
+const createLivingCondition = async (req, res) => {
+    const { user, sleep_attitude, major, cleaniness_score } = req.body;
+    try {
+        const condition = await LivingConditions.create({ user, sleep_attitude, major, cleaniness_score });
+        res.status(200).json(condition);
+    } catch (error) {
+        res.status(400).json({ error: error.message });
+    }
+};
+
+// Delete a living condition
+const deleteLivingCondition = async (req, res) => {
+    const { id } = req.params;
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+        return res.status(400).json({ error: 'No such living condition' });
+    }
+    const condition = await LivingConditions.findOneAndDelete({ _id: id });
+    if (!condition) {
+        return res.status(400).json({ error: 'No such living condition' });
+    }
+    res.status(200).json(condition);
+};
+
+// Update a living condition
+const updateLivingCondition = async (req, res) => {
+    const { id } = req.params;
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+        return res.status(400).json({ error: "No such living condition" });
+    }
+    const condition = await LivingConditions.findOneAndUpdate({ _id: id }, {
+        ...req.body
+    }, { new: true });
+    if (!condition) {
+        return res.status(400).json({ error: "No such living condition" });
+    }
+    res.status(200).json(condition);
+};
+
+module.exports = {
+    getLivingConditions,
+    getLivingCondition,
+    createLivingCondition,
+    deleteLivingCondition,
+    updateLivingCondition
+};
