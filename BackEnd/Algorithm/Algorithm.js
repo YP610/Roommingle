ff19b9b402faf3eb06ceccd8502eb213e2199a73
YP10 <144463548+YP10@users.noreply.github.com>
2025-04-02 21:40:39 -0400

    sort

--- BackEnd/Algorithm/Algorithm.js
+++ BackEnd/Algorithm/Algorithm.js
@@ -1,4 +1,4 @@
-const userMap{
+const userMap={
     maleFreshman:[],
     femaleFreshman:[],
     H_maleF:[],
@@ -7,4 +7,50 @@ const userMap{
     femaleNF:[],
     H_maleNF:[],
     H_femaleNF:[]
+};
+
+function addUser(user){
+    const gender=user.gender==="male"?"male":"female";
+    const freshman=user.is_freshman;
+    const honors=user.is_honors;
+
+    let groupKey;
+
+
+    if(!honors){
+        if(!freshman){
+            if(gender==="male"){
+                groupKey="maleNF";
+            }
+            else{
+                groupKey="femaleNF";
+            }
+        }
+        else if(gender==="male"){
+            groupKey="maleFreshman";
+        }
+        else{
+            groupKey="femaleFreshman";
+        }
+    }
+    else if(!freshman){
+        if(gender==="male"){
+            groupKey="H_maleNF";
+        }
+        else{
+            groupKey="H_femaleNF";
+        }
+    }
+    else if(gender==="male"){
+        groupKey="H_maleFreshman";
+    }
+    else{
+        groupKey="H_femaleFreshman";
+    }
+    userMap[groupKey].push(user);
+
+}
+
+module.exports={
+    addUser
 };
