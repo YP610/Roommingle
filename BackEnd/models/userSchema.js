649c91e48677564cd9e086aaab50b4ff2a803b37
mlachtara <mlachtara@umass.edu>
2025-04-09 20:57:28 -0400

    Changing schema to make everything nest uner userschema to fix userid issue

--- BackEnd/models/userSchema.js
+++ BackEnd/models/userSchema.js
@@ -1,11 +1,37 @@
-const mongoose=require("mongoose");
-const Schema=mongoose.Schema;
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const UserSchema = new Schema({
+    // User Basic Info
+    name: { type: String, required: true },
+    email: { type: String, required: true },
+    password: { type: String, required: true },
 
-const UserSchema=new mongoose.Schema({
-    name:{type:String, required: true},
-    email:{type:String, required: true},
-    password:{type:String, required: true},
-},{ timestamps:true});
+    // Contact Information
+    contact: {
+        number: { type: String, required: false },
+        snap: { type: String, required: false },
+        insta: { type: String, required: false },
+    },
 
-const user = mongoose.model('User', UserSchema);
-module.exports=user;
+    // Feed Information
+    feed: {
+        is_freshman: { type: Boolean, required: true },
+        gender: { type: String, enum: ["male", "female"], required: true },
+        is_honors: { type: Boolean, required: true },
+        residence_hall: { type: String, required: true },
+    },
+
+    // Hobbies
+    hobbies: { type: String, required: false },
+
+    // Living Conditions
+    living_conditions: {
+        sleep_attitude: { type: String, required: true },
+        major: { type: String, required: true },
+        cleanliness_score: { type: Number, required: true },
+    }
+
+}, { timestamps: true });
+
+const User = mongoose.model('User', UserSchema);
+module.exports = User;
